7116ebded1f595ab5ebe3f30e23ea7525c67432b
vansh1293 <aroravansh.com@gmail.com>
2025-06-17 00:00:00 +0530

    added function to fetch upcoming codechef and codeforce contests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
-
+import getCodeForces from "@/helpers/getCodeForces";
+import { get } from "http";
 export default function Home() {
   redirect("/dashboard");
   return null;
