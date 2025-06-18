a7cf136c403665a40eb39adcd0e1a07332fd3758
Shaurya Rahlon <shauryarahlon.11@gmail.com>
2025-06-19 03:13:36 +0530

    feat: implement password reset functionality with email notification

---
+++ src/helpers/sendPasswordResetEmail.ts
@@ -0,0 +1,48 @@
+import { ApiResponse } from "@/types/ApiResponse";
+import nodemailer from "nodemailer";
+import { render } from "@react-email/render";
+import ResetPassEmail from "../../emails/ResetPassEmail";
+
+interface MailOptions {
+  from: string | undefined;
+  to: string;
+  subject: string;
+  html: string;
+}
+export async function sendPasswordResetEmail(
+  email: string,
+  username: string,
+  resetCode: string
+): Promise<ApiResponse> {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.NODEMAILER_EMAIL, //fix this according to env
+        pass: process.env.NODEMAILER_PASS, //this too
+      },
+    });
+
+    const htmlcontent = await render(ResetPassEmail({ username, resetCode }));
+
+    const mailOptions: MailOptions = {
+      from: process.env.NODEMAILER_EMAIL,
+      to: email,
+      subject: "🔐 Mystery Message Reset OTP",
+      html: htmlcontent,
+    };
+    await transporter.sendMail(mailOptions);
+    return {
+      success: true,
+      message: "Reset email sent successfully",
+    };
+  } catch (error) {
+    console.error("Failed to sent verification Email ", error);
+    return {
+      success: false,
+      message: "Failed to send reset email",
+    };
+  }
+}
--- src/model/User.ts
+++ src/model/User.ts
@@ -1,28 +1,41 @@
-import mongoose, { Document, Schema } from 'mongoose';
-
+import mongoose, { Document, Schema } from "mongoose";
 
 export interface User extends Document {
-    username: string,
-    password: string,
-    email: string,
-    isVerified: boolean,
-    verifyCode: string,
-    verifyCodeExpires: Date,
-    acceptingContest: boolean
+  username: string;
+  password: string;
+  email: string;
+  isVerified: boolean;
+  verifyCode: string;
+  verifyCodeExpires: Date;
+  acceptingContest: boolean;
+  resetCode?: string;
+  resetCodeExpires?: Date;
 }
 
 const UserSchema: Schema<User> = new Schema({
-    username: { type: String, required: [true, "Username is required"], trim: true },
-    email: {
-        type: String, required: [true, "Email is required"], match: [/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/, "Please use a valid email address"]
-    },
-    password: { type: String, required: [true, "Password is required"] },
-    verifyCode: { type: String },
-    verifyCodeExpires: { type: Date },
-    isVerified: { type: Boolean, default: false },
-    acceptingContest: { type: Boolean, default: false }
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    match: [
+      /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+      "Please use a valid email address",
+    ],
+  },
+  password: { type: String, required: [true, "Password is required"] },
+  verifyCode: { type: String },
+  verifyCodeExpires: { type: Date },
+  isVerified: { type: Boolean, default: false },
+  acceptingContest: { type: Boolean, default: false },
+  resetCode: { type: String },
+  resetCodeExpires: { type: Date },
 });
 
-const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>('User', UserSchema);
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
 export default UserModel;
-
